7b4cb4a51fea2aab46ad016c0368dec7b9bd46d2
Alina13A <alinaarta@yandex.ru>
2025-01-14 18:21:15 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,7 @@
+
+username = input ('Имя пользователя: ')
+title = input ('Заголовок: ')
+content = input ('Описание: ')
+status = input ('Статус: ')
+created_date = input ('Дата создания в формате "день-месяц-год": ')
+issue_date = input ('Дата окончания в формате "день-месяц-год: ')
