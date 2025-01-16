2954ed752fca2533a8e2434bce6413d227b8c229
Alina13A <alinaarta@yandex.ru>
2025-01-16 18:40:05 +0300

    Add files via upload

---
+++ final.py
@@ -0,0 +1,25 @@
+
+username = input('Имя пользователя: ')
+titles = ([input('Заголовок 1: '), input('Заголовок 2: '), input('Заголовок 3: ')])
+content = input('Описание: ')
+status = input('Статус: ')
+created_date = input('Дата создания заметки в формате "день-месяц-год": ')
+issue_date = input('Дата истечения заметки (дедлайн) в формате "день-месяц-год": ')
+
+
+note= [username,
+       titles,
+       content,
+       status,
+       created_date,
+       issue_date]
+
+
+print(f'Имя пользователя: ', note[0])
+print(f'Заголовок: ', note[1])
+print(f'Описание: ', note[2])
+print(f'Статус: ', note[3])
+print(f'Дата создания заметки в формате "день-месяц-год": {created_date [0:-5]}')
+print(f'Дата истечения заметки (дедлайн) в формате "день-месяц-год": {issue_date [0:-5]}')
+
+
