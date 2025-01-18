739710bfe196e5d2e82f47682151e81526b80535
Alina13A <alinaarta@yandex.ru>
2025-01-18 18:00:07 +0300

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,6 +1,6 @@
 
 created_date = input ('Введите дату создания заметки: ')
-print (f'Дата создания заметки: {created_date [0:-5]}')
+print (f'Дата создания заметки: {created_date [0:5]}')
 
 issue_date = input ('Введите дату истечения заметки: ')
-print (f'Дата истечения заметки: {issue_date [0:-5]}')
+print (f'Дата истечения заметки: {issue_date [0:5]}')
