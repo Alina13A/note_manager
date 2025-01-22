05aa49b9c1bd1b23ae8560bfa9f56e778dd232ec
Alina13A <alinaarta@yandex.ru>
2025-01-22 16:48:49 +0300

    Add files via upload

---
+++ add_titles_loop.py
@@ -0,0 +1,34 @@
+
+username = input('Имя пользователя: ')
+titles = ([input('Заголовок 1: '), input('Заголовок 2: '), input('Заголовок 3: ')])
+
+while True:
+
+    title_new = input('Введите заголовок или оставьте пустым для завершения: ')
+
+    if title_new == "":
+        print("Завершение ввода заголовков")
+        break
+    titles.append(title_new)
+
+content = input('Описание: ')
+status = input('Статус: ')
+created_date = input('Дата создания заметки в формате "день-месяц-год": ')
+issue_date = input('Дата истечения заметки (дедлайн) в формате "день-месяц-год": ')
+
+note = [username,
+        titles,
+        content,
+        status,
+        created_date,
+        issue_date]
+
+print('Имя пользователя: ', note[0])
+print('Заголовки: ', note[1])
+print('Описание: ', note[2])
+print('Статус: ', note[3])
+print(f'Дата создания заметки в формате "день-месяц-год": {created_date[0:5]}')
+print(f'Дата истечения заметки (дедлайн) в формате "день-месяц-год": {issue_date[0:5]}')
+
+
+
